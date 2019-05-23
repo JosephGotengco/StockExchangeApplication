6eae9f279e9a8e93b9028ac9ee565e61632bbc04
Joseph Gotengco <45640953+JosephGotengco@users.noreply.github.com>
2019-05-22 23:18:26 -0700

    Bcrypt was patched!!! So password hashing now works!
    A Bcrypt developer deleted a file but it was included in a patch that came out AFTER THE PRESENTATIONS. But at least it came out and we can deliver like promised!

--- test/test.passwordhashing.js
+++ test/test.passwordhashing.js
@@ -1,54 +1,54 @@
-// const bcrypt = require('bcrypt');
-// const assert = require('chai').assert;
+const bcrypt = require('bcrypt');
+const assert = require('chai').assert;
 
-// var saltRounds = 10;
+var saltRounds = 10;
 
 
-// describe('encrpytPass', function() {
-// 	it('Passwords should match and return true', function(){
-// 		passwords = ['abc123', '123abc', '11111', 'aaaaaa', 'asdakjndksdnv'];
-// 		for(let i =0; i < passwords.length;i++) {
-// 			bcrypt.hash(passwords[i], saltRounds, function(err, hash) {
-// 				if(err){
-// 					console.log("Password did not hash correctly");
-// 				}else {
-// 					bcrypt.compare(passwords[i], hash, function(err, result) {
+describe('encrpytPass', function() {
+	it('Passwords should match and return true', function(){
+		passwords = ['abc123', '123abc', '11111', 'aaaaaa', 'asdakjndksdnv'];
+		for(let i =0; i < passwords.length;i++) {
+			bcrypt.hash(passwords[i], saltRounds, function(err, hash) {
+				if(err){
+					console.log("Password did not hash correctly");
+				}else {
+					bcrypt.compare(passwords[i], hash, function(err, result) {
 
-// 						if(err) {
-// 							console.log(err);
-// 						}
-// 						if(result) {
+						if(err) {
+							console.log(err);
+						}
+						if(result) {
 
-// 							assert.isTrue(result);
-// 						}
-// 					})
-// 				}
-// 			})
-// 		}
-// 	});
-// 	it("Passwords shouldn't match and return false", function(){
-// 		passwords = ['abc123', '123abc', '11111', 'aaaaaa', 'asdakjndksdnv'];
-// 		wrongPasswords = ['asdsasdasd', 'asdasfasa', 'afasfafsa', 'xcvxcvxc', '5346546']
-// 		for(let i =0; i < passwords.length;i++) {
-// 			bcrypt.hash(passwords[i], saltRounds, function(err, hash) {
-// 				if(err){
-// 					console.log("Password did not hash correctly");
-// 				}else {
+							assert.isTrue(result);
+						}
+					})
+				}
+			})
+		}
+	});
+	it("Passwords shouldn't match and return false", function(){
+		passwords = ['abc123', '123abc', '11111', 'aaaaaa', 'asdakjndksdnv'];
+		wrongPasswords = ['asdsasdasd', 'asdasfasa', 'afasfafsa', 'xcvxcvxc', '5346546']
+		for(let i =0; i < passwords.length;i++) {
+			bcrypt.hash(passwords[i], saltRounds, function(err, hash) {
+				if(err){
+					console.log("Password did not hash correctly");
+				}else {
 
-// 					bcrypt.compare(wrongPasswords[i], hash, function(err, result) {
-// 						if(err) {
-// 							console.log(err);
-// 						}
-// 						if(result) {
+					bcrypt.compare(wrongPasswords[i], hash, function(err, result) {
+						if(err) {
+							console.log(err);
+						}
+						if(result) {
 
-// 							assert.isFalse(result);
-// 						}
-// 						else{
-// 							assert.isFalse(result);
-// 						}
-// 					})
-// 				}
-// 			})
-// 		}
-// 	})
-// })
+							assert.isFalse(result);
+						}
+						else{
+							assert.isFalse(result);
+						}
+					})
+				}
+			})
+		}
+	})
+})
